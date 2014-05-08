e2897f76022a86a0fa7c6b06d47f1580ace428e7
Nichlas F. Nielsen <nichlasnielsen@hotmail.com>
2014-05-08 13:48:24 +0200

    added models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from gallery.models import Album, Picture
 
-# Register your models here.
+class PictureInline(admin.TabularInline):
+	model = Picture
+	extra = 1
+
+class AlbumAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,				{'fields': ['album_name']}),
+		('Date information',{'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [PictureInline]
+
+	list_display = ('album_name', 'pub_date', 'was_published_recently')
+#Album.objects.get().picture_set.count(),
+admin.site.register(Album, AlbumAdmin)
--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,36 @@
+import datetime
+import os
 from django.db import models
+from django.utils import timezone
+
 
 class Album(models.Model):
 	album_name = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('Date published')
+#	os.mkdir(os.path.join('/gallery/media/images', models.ForeignKey(Album)))
+
+	def __unicode__(self):
+		return self.album_name
+
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date < now
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 class Picture(models.Model):
+	album = models.ForeignKey(Album)
 	pic_name = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('Date published')
+#	pic_loc = models.FileField(upload_to='/media/images/'+str(album))
+
+	def __unicode__(self):
+		return self.pic_name
+
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date < now
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
